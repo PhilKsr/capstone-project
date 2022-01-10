1d05b96df4ecdcc69bbf69491dbc6270b442114b
Philipp Kaiser <p.kaiser95@gmx.de>
2022-01-10 08:55:07 +0100

    Added navigationbar and react routes to navigate through app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   const [myHeadline, setMyHeadline] = useState("");
@@ -17,6 +19,20 @@ function App() {
   return (
     <div className='App'>
       <h1>Message from Server: {myHeadline}</h1>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<h1>HOME should be displayed</h1>} />
+        <Route path='new' element={<h1>NEW should be displayed</h1>} />
+        <Route
+          path='collections'
+          element={<h1>COLLECTIONS should be displayed</h1>}
+        />
+        <Route
+          path='discover'
+          element={<h1>DISCOVER should be displayed</h1>}
+        />
+        <Route path='profile' element={<h1>PROFILE should be displayed</h1>} />
+      </Routes>
     </div>
   );
 }
