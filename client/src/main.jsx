d83eb5e24950fd9698736077ffd889c2dd83d1f4
Philipp Kaiser <p.kaiser95@gmx.de>
2022-01-10 16:42:16 +0100

    Added global styles from styled-components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "./GlobalStyles";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
